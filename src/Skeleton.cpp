8e7963652442d4958067cdacbb1ab66b2e848891
Falucskai András <86012066+Andros383@users.noreply.github.com>
2026-04-23 15:28:43 +0200

    Harmadik házi kezdete, nem fogok új repót csinálni mert lusta vagyok.

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -1,37 +1,8 @@
 //=============================================================================================
-// Mintaprogram: Zold h�romszog. Ervenyes 2025.-tol
-//
-// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat, BOM kihuzando.
-// Tilos:
-// - mast "beincludolni", illetve mas konyvtarat hasznalni
-// - faljmuveleteket vegezni a printf-et kiveve
-// - mashonnan atvett programresszleteket forrasmegjeloles nelkul felhasznalni, ide�rtve ChatGPT-t �s t�rsait is
-// - felesleges programsorokat a beadott programban hagyni
-// - felesleges kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
-// ---------------------------------------------------------------------------------------------
-// A feladatot ANSI C++ nyelvu forditoprogrammal ellenorizzuk, a Visual Studio-hoz kepesti elteresekrol
-// es a leggyakoribb hibakrol (pl. ideiglenes objektumot nem lehet referencia tipusnak ertekul adni)
-// a hazibeado portal ad egy osszefoglalot.
-// ---------------------------------------------------------------------------------------------
-// A feladatmegoldasokban csak olyan OpenGL es GLM fuggvenyek hasznalhatok, amelyek az oran a feladatkiadasig elhangzottak
-//
-// NYILATKOZAT
-// ---------------------------------------------------------------------------------------------
-// Nev    : Falucskai Andras
-// Neptun : A2PT9J
-// ---------------------------------------------------------------------------------------------
-// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
-// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
-// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a
-// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb
-// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem,
-// azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
-// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.
-// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
-// negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
-//=============================================================================================//=============================================================================================
-
+// Textúra leképzés
+//=============================================================================================
 #include "framework.h"
+<<<<<<< HEAD
 
 class Camera2D {
 	vec2 wCenter;
@@ -66,6 +37,10 @@ public:
 		return vec2(translated.x, translated.y);
 	}
 };
+=======
+#include <cstdio>
+#include <cstdlib>
+>>>>>>> 7417295 (Harmadik házi kezdete, nem fogok új repót csinálni mert lusta vagyok.)
 
 const char* vertSource = R"(
 	#version 330
@@ -73,10 +48,8 @@ const char* vertSource = R"(
 
 	layout(location = 0) in vec2 vertexPosition;
 
-	uniform mat4 MVP;
-
 	void main() {
-		gl_Position = MVP * vec4(vertexPosition, 0, 1);
+		gl_Position = vec4(vertexPosition, 0, 1);
 	}
 )";
 
@@ -92,9 +65,14 @@ const char* fragSource = R"(
 	}
 )";
 
+// csúcspont árnyaló
+const char * vertSourceText = R"(
+	#version 330
 
-const int winWidth = 600, winHeight = 600;
+	layout(location = 0) in vec2 vertexXY;	// Attrib Array 0
+	layout(location = 1) in vec2 vertexUV;			// Attrib Array 1
 
+<<<<<<< HEAD
 class CatmullRomSpline {
 public:
 	Geometry<vec2>  draw_curve;
@@ -345,18 +323,169 @@ public:
 	}
 	BallApp() : glApp("Ball App") {}
 
+=======
+	out vec2 texCoord;								// output attribute
+
+	void main() {
+		texCoord = vertexUV;														// copy texture coordinates
+		gl_Position = vec4(vertexXY, 0, 1);
+	}
+)";
+
+// pixel árnyaló
+const char * fragSourceText = R"(
+	#version 330
+
+	uniform sampler2D textureUnit;
+
+	in vec2 texCoord;			// variable input: interpolated texture coordinates
+	out vec4 outColor;		// output that goes to the raster memory as told by glBindFragDataLocation
+
+	void main() { outColor = texture(textureUnit, texCoord); }
+)";
+
+
+const int winWidth = 960, winHeight = 540;
+
+std::vector<int16_t> compressed_image = {2308, 13, 84, 5, 152, 17, 76, 17, 144, 29, 64, 25, 136, 45, 16, 5, 32, 29, 132, 65,
+4, 21, 8, 33, 128, 85, 12, 41, 120, 93, 4, 73, 20, 9, 52, 101, 4, 81, 4, 29, 12, 5,
+12, 1129, 14, 241, 18, 241, 18, 245, 14, 145, 6, 97, 18, 125, 6, 9, 10, 97, 18, 53,
+10, 61, 30, 93, 22, 45, 14, 61, 30, 93, 22, 45, 18, 5, 6, 49, 30, 93, 30, 37, 22,
+61, 18, 97, 30, 37, 22, 61, 14, 97, 22, 9, 6, 37, 22, 45, 6, 9, 6, 9, 10, 93, 26,
+33, 6, 5, 26, 41, 6, 5, 10, 5, 6, 101, 26, 33, 42, 49, 6, 101, 14, 5, 6, 41, 46, 21,
+6, 17, 6, 9, 6, 93, 14, 53, 50, 29, 14, 105, 6, 9, 10, 41, 54, 13, 34, 93, 14, 13,
+6, 41, 54, 9, 42, 85, 30, 49, 26, 21, 58, 81, 34, 45, 26, 21, 62, 5, 6, 65, 42, 69,
+6, 13, 62, 73, 50, 41, 10, 5, 14, 13, 66, 69, 42, 9, 6, 37, 106, 61, 58, 37, 106,
+61, 34, 5, 18, 37, 6, 13, 90, 13, 6, 25, 10, 17, 30, 9, 10, 57, 98, 33, 30, 5, 26,
+33, 4, 33, 102, 9, 18, 13, 50, 5, 6, 9, 10, 9, 6, 4, 17, 4, 17, 122, 5, 6, 5, 46, 5,
+8, 9, 10, 9, 6, 8, 37, 82, 5, 10, 5, 10, 5, 14, 9, 38, 5, 24, 6, 13, 6, 16, 29, 18,
+9, 34, 5, 58, 17, 10, 13, 6, 5, 36, 17, 12, 41, 6, 17, 6, 5, 74, 53, 32, 17, 20, 57,
+6, 5, 50, 5, 6, 5, 6, 57, 32, 21, 20, 61, 18, 5, 22, 5, 18, 5, 6, 61, 28, 17, 24,
+61, 4, 6, 5, 6, 21, 4, 21, 6, 65, 4, 9, 20, 9, 28, 29, 4, 45, 6, 121, 24, 5, 32, 29,
+4, 37, 6, 5, 6, 121, 20, 9, 28, 205, 20, 5, 28, 81, 4, 125, 16, 5, 24, 5, 4, 209,
+12, 5, 4, 5, 16, 217, 8, 17, 16, 913};
+
+std::vector<vec3> uncompressed_image;
+
+void uncompress(){
+	printf("Size of image: %lu\n", compressed_image.size());
+	for(int16_t elem : compressed_image){
+		bool print = false;
+		if(elem == 13) print = true;
+		int16_t mask = 0b0000000000000011;
+		int16_t lower = elem & mask;
+		int upper = (elem & (~mask)) >> 2;
+
+		vec3 color = vec3(-1, -1, -1);
+		switch (lower) {
+			case 0b00:
+				color = vec3(1, 1, 1);
+				break;
+			case 0b01:
+				color = vec3(0, 0, 1);
+				break;
+			case 0b10:
+				color = vec3(0, 1, 0);
+				break;
+			case 0b11:
+				color = vec3(0, 0, 0);
+				break;
+		}
+
+		if(color.x < 0){
+			printf("Invalid color code\n");
+			exit(1);
+		}
+
+		printf("Pushing %d db %.2f %.2f %.2f\n", upper, color.x, color.y, color.z);
+
+		for(int i = 0; i<upper; i++){
+			uncompressed_image.push_back(color);
+		}
+
+
+		// if(print){
+		// 	printf("%x\n", elem);
+		// 	printf("%x\n", lower);
+		// 	printf("%x\n", upper);
+		// }
+	}
+	printf("uncompressed size: %lu\n", uncompressed_image.size());
+}
+
+struct WorldMap {
+	unsigned int vao, vbo[2];	// vao és két vbo: geometria + uv
+	std::vector<vec2> vtx;	    // geometria a CPU-n
+	Texture texture;            // kép a tapétázáshoz
+	int picked = -1;		    // kiválasztott csúcspont sorszáma
+	WorldMap() : texture(64, 64){
+		texture.updateTexture(64, 64, uncompressed_image);
+
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+		glGenBuffers(2, &vbo[0]);  // egyszerre két vbo-t kérünk
+		// a négyszög csúcsai kezdetben normalizált eszközkoordinátákban
+		vtx = { vec2(-1, -1), vec2(1, -1), vec2(1, 1), vec2(-1, 1) };
+		updateGPU();               // GPU-ra másoljuk
+		glEnableVertexAttribArray(0); // a 0. vbo a 0. bemeneti regisztert táplálja
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL); // csúcsonként 2 float-tal
+		// a négyszög csúcsai textúratérben
+		// std::vector<vec2> uvs = { vec2(0, 1), vec2(1, 1), vec2(1, 0), vec2(0, 0) };
+		std::vector<vec2> uvs = { vec2(0, 0), vec2(1, 0), vec2(1, 1), vec2(0, 1) };
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[1]); // GPU-ra másoljuk
+		glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(vec2), &uvs[0], GL_STATIC_DRAW);
+		glEnableVertexAttribArray(1); // a 1. vbo a 1. bemeneti regisztert táplálja
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, NULL); // csúcsonként 2 float-tal
+	}
+	void updateGPU() { // vtx tömb átmásolása a GPU-ra a vbo[0] VBO-ba
+		glBindVertexArray(vao);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+		glBufferData(GL_ARRAY_BUFFER, vtx.size() * sizeof(vec2), &vtx[0], GL_DYNAMIC_DRAW);
+	}
+	void Draw(GPUProgram* gpuProgram) {
+		int textureUnit = 0; // textúra mintavevő egység
+		gpuProgram->setUniform(textureUnit, "textureUnit"); // türkisz nyíl
+		texture.Bind(textureUnit);                          // piros nyíl
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLE_FAN, 0, 4);        // négyszög rajzolás
+	}
+};
+
+class TextureApp : public glApp {
+	Geometry<vec2> *line;
+	WorldMap* wm;
+	GPUProgram* gpuProgram;
+	GPUProgram* gpuProgramText;
+	bool mousePressed = false;
+public:
+	TextureApp() : glApp(3, 3, winWidth, winHeight, "Texturing") { }
+>>>>>>> 7417295 (Harmadik házi kezdete, nem fogok új repót csinálni mert lusta vagyok.)
 	void onInitialization() {
 		gpuProgram = new GPUProgram(vertSource, fragSource);
+		gpuProgramText = new GPUProgram(vertSourceText, fragSourceText);
+		glClearColor(0, 0, 0, 0);     // háttér fekete
 
+<<<<<<< HEAD
 		glViewport(0, 0, winHeight, winWidth);
 
+=======
+>>>>>>> 7417295 (Harmadik házi kezdete, nem fogok új repót csinálni mert lusta vagyok.)
 		glLineWidth(3);
-		glPointSize(10);
 
+<<<<<<< HEAD
 		spline = new CatmullRomSpline();
-	}
+=======
+		uncompress();
 
+		wm = new WorldMap();
+		line = new Geometry<vec2>;
+		line->Vtx().push_back(vec2(0, 0));
+		line->Vtx().push_back(vec2(1, 0));
+		line->updateGPU();
+>>>>>>> 7417295 (Harmadik házi kezdete, nem fogok új repót csinálni mert lusta vagyok.)
+	}
 	void onDisplay() {
+<<<<<<< HEAD
 		glClearColor(0, 0, 0, 0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
@@ -376,8 +505,22 @@ public:
 			b->updateFromTime(endTime - startTime, &spline->draw_curve.Vtx());
 		}
 
+=======
+		glClear(GL_COLOR_BUFFER_BIT); // törlés
+		glViewport(0, 0, winWidth, winHeight);
+		gpuProgramText->Use();
+		wm->Draw(gpuProgramText);
+		gpuProgram->Use();
+		line->Draw(gpuProgram, GL_LINES, vec3(1, 0, 0));
+	}
+	void onMousePressed(MouseButton button, int pX, int pY) {
+	}
+	void onMouseReleased(MouseButton button, int pX, int pY) {
+	}
+	void onMouseMotion(int pX, int pY) {
+>>>>>>> 7417295 (Harmadik házi kezdete, nem fogok új repót csinálni mert lusta vagyok.)
 		refreshScreen();
 	}
 };
 
-BallApp app;
+TextureApp app;
